f46dc4660347e978cf72563ade3a716b41c97b92
Jason Walton <jwalton@benbria.ca>
2013-12-23 11:04:28 -0500

    1.0.1: Switch to browserify-transform-tools implementation.  Use async call for reading file for performance.

--- index.js
+++ index.js
@@ -1,69 +1,45 @@
 // Based loosely on https://github.com/quarterto/rfolderify
 
+var transformTools = require('browserify-transform-tools');
+
 var path = require('path');
 var fs = require('fs');
 
-var through = require('through');
-var falafel = require('falafel');
+module.exports = transformTools.makeFalafelTransform("includify",
+    function (node, transformOptions, done) {
+        if (node.type === 'CallExpression' &&
+            node.callee.type === 'Identifier' &&
+            node.callee.name === 'includify') {
 
-var defaults = {require:"require"}
+            var file = transformOptions.file;
 
-module.exports = function (file) {
-    if (/\.json$/.test(file)) return through();
-
-    var data = '';
-    var dirname = path.dirname(file);
-    var varNames = ['__filename', '__dirname', 'path', 'join'];
-    var vars = [file, dirname, path, path.join];
-    var pending = 0;
-
-    var tr = through(write, end);
-    return tr;
-
-    // Read all the data into `buf`
-    function write (buf) { data += buf }
-
-    // Called when we're done reading data
-    function end () {
-        try {
-            var output = parse();
-        } catch (err) {
-            this.emit('error', new Error(
-                err.toString().replace('Error: ', '') + ' (' + file + ')')
-            );
-        }
-        if(pending == 0) finish(output);
-    }
-
-    function finish(output) {
-        tr.queue(String(output));
-        tr.queue(null);
-    }
-
-    function parse() {
-        var output = falafel(data, function (node) {
-            if (node.type === 'CallExpression' &&
-                node.callee.type === 'Identifier' &&
-                node.callee.name === 'includify') {
-                ++pending;
-
-                // Parse arguemnts to calls to `includify(file)`
-                var args = node.arguments;
-                for (var i = 0; i < args.length; i++) {
-                    var t = 'return ' + (args[i]).source();
-                    args[i] = Function(varNames, t).apply(null, vars);
-                }
-
-                // Resolve the file to require relative the file which is requiring it
-                var parentDir = path.dirname(file);
-                var includeFile = path.resolve(parentDir, args[0])
-
-                content = fs.readFileSync(includeFile, "utf-8");
-                node.update(content);
+            // Parse arguemnts to calls to `includify(file)`
+            var dirname = path.dirname(file);
+            var varNames = ['__filename', '__dirname', 'path', 'join'];
+            var vars = [file, dirname, path, path.join];
+            var args = node.arguments;
+            for (var i = 0; i < args.length; i++) {
+                var t = 'return ' + (args[i]).source();
+                args[i] = Function(varNames, t).apply(null, vars);
             }
-        });
 
-        finish(output);
-        return output;
+            // Resolve the file to require relative the file which is requiring it
+            var includeFile = path.resolve(dirname, args[0])
+
+            if(transformOptions.config && transformOptions.config.verbose) {
+                console.log("includify - " + file + ": including " + includeFile);
+            }
+
+            fs.readFile(includeFile, "utf-8", function(err, content) {
+                if(err) {
+                    return done(err);
+                } else {
+                    node.update(content);
+                    done();
+                }
+            });
+        } else {
+            done();
+        }
     }
-};
+);
